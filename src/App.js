fd2cc7843e491282277156c13858972e2546814e
Beriwan Tepe <tepe.1@win.tu-berlin.de>
2018-12-21 14:36:14 +0100

    added comment

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      // bla
+      // blabla
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
