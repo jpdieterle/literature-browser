bc4f2ec3e8ab4215b64b0961b7c045ee3e3cbfd3
Beriwan Tepe <tepe.1@win.tu-berlin.de>
2019-02-03 14:35:32 +0100

    changed favicon, added credits to about

--- src/components/views/About.js
+++ src/components/views/About.js
@@ -10,7 +10,14 @@ class About extends React.Component {
   render() {
     const { classes } = this.props;
     return(
-      <div>Ich bin die About Seite!</div>
+      <div>
+        Ich bin die About Seite!
+        <div>
+          Favicon made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a>
+          from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by
+          <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
+        </div>
+      </div>
     );
   }
 }
