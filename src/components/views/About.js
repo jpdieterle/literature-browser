823d216ef586d5d60b1690209165633d21f7d537
Beriwan Tepe <tepe.1@win.tu-berlin.de>
2019-02-10 12:48:07 +0100

    styled About

--- src/components/views/About.js
+++ src/components/views/About.js
@@ -13,8 +13,8 @@ class About extends React.Component {
       <div className={classes.root}>
         Ich bin die About Seite!
         <div>
-          Favicon made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a>
-          from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by
+          Favicon made by {' '}<a href="https://www.freepik.com/" title="Freepik">Freepik</a>{' '}
+          from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by{' '}
           <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank" rel="noopener noreferrer">CC 3.0 BY</a>
         </div>
       </div>
