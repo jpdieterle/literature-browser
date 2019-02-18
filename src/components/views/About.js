2fd47eb0bd91165aea6c369484dbd4bdde816ecc
Beriwan Tepe <tepe.1@win.tu-berlin.de>
2019-02-18 23:26:14 +0100

    added about text

--- src/components/views/About.js
+++ src/components/views/About.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 
 // about the project, impressum
 class About extends React.Component {
@@ -12,12 +13,21 @@ class About extends React.Component {
     const { classes } = this.props;
     return(
       <div className={classes.root}>
-        Ich bin die About Seite!
-        <div>
+        <Typography variant={'h5'} color={'primary'} className={classes.aboutText}>Über diese Seite</Typography><br/>
+        <Typography className={classes.aboutText} paragraph={true}>
+          Diese Webseite ist das Ergebnis des Projekts “Gutenberg German Poetry Corpus” im Rahmen des Moduls “Interdisziplinäres Medienprojekt” des Studiengangs B.Sc. Medieninformatik an der Freien Universität Berlin und Technischen Universität Berlin.
+          Die Motivation dieses Projektes ist es, die Arbeit von beispielsweise Linguist*innen und Psycholog*innen zu erleichtern, um Erkenntnisse über die in Gedichten verwendete Sprache zu erlangen.
+          Das Ergebnis ist diese Webanwendung, die es ermöglicht auf die Gedichte des  “Gutenberg-Korpus” (des größten deutschsprachigen Gedichts-Korpus) zuzugreifen, diese nach bestimmten Kriterien zu filtern und weitere Texte hinzuzufügen.
+        </Typography>
+        <Typography className={classes.aboutText} paragraph={true}>
+          Projektleiterin war Frau Dr. Jana Lüdtke, wissenschaftliche Mitarbeiterin im Fachgebiet Allgemeine und neurokognitive Psychologie an der Freien Universität Berlin. Assistiert hat Clarissa Elisabeth Staudt, Studentin der Medieninformatik an der TU und FU Berlin.
+          Am Projekt mitgewirkt haben Anna-Liza Beriwan Tepe, Arne Kuhle, Flora Muscinelli, Fatih Tekin und Ioanna Vasiliou.
+        </Typography>
+        <Typography className={classes.aboutText}>
           Favicon made by {' '}<a href="https://www.freepik.com/" title="Freepik">Freepik</a>{' '}
           from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by{' '}
           <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank" rel="noopener noreferrer">CC 3.0 BY</a>
-        </div>
+        </Typography>
       </div>
     );
   }
@@ -29,7 +39,11 @@ About.propTypes = {
 
 const styles = theme => ({
   root:{
-    marginLeft: theme.spacing.unit * 4
+    margin: theme.spacing.unit * 4
+  },
+  aboutText:{
+    minWidth: 500,
+    maxWidth: 1000,
   }
 });
 
