e496b4c6cc724ffd25963154c336bb8eefcf18a9
Beriwan Tepe <tepe.1@win.tu-berlin.de>
2019-03-04 23:55:40 +0100

    file upload replaces text input

--- src/components/views/admin/tabs/TextManagement.js
+++ src/components/views/admin/tabs/TextManagement.js
@@ -11,10 +11,11 @@ import NotificationContext from '../../../notifications/NotificationContext';
 // add new texts to server database, start importing texts from external database on server, view import status
 class TextManagement extends Component {
   state= {
-    newText: '',
     loading: false,
     lastImportTime: 'unbekannt',
-    importStatus: 'unbekannt'
+    importStatus: 'unbekannt',
+    selectedFiles: null,
+    progress: 0,
   };
 
   // see if server is currently importing or when last import was started
@@ -62,14 +63,9 @@ class TextManagement extends Component {
     });
   };
 
-  // update state when new text is entered into input field
-  handleTextChange = (event) => {
-    this.handleChange('newText', event.target.value);
-  };
-
-  // delete entered text
-  emptyTextField = () => {
-    this.handleChange('newText', '');
+  // update state when file selection changes
+  handleFileChange = (event) => {
+    this.handleChange('selectedFiles', event.target.value);
   };
 
   // update authors, genres, time range
@@ -115,77 +111,51 @@ class TextManagement extends Component {
 
   // request server to add input from textfield as new text to database
   requestAddText = () => {
+    // check if file has been selected
+    if(this.state.selectedFiles === null) {
+      this.context.handleNotificationChange(true, 'Bitte wählen Sie zuerst eine Datei aus.', 'addText', 'error');
+      return;
+    }
+
     this.handleChange('loading', true);
+
+    let data = new FormData();
+    data.append('file', this.state.selectedFiles);
+    data.append('id', localStorage.getItem('sessionID'));
+    data.append('addText', 'true');
+
     fetch(' /backend/lib/admin.php',{
       method: 'POST',
       credentials: 'same-origin', // allow cookies -> session management
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        addText: true,
-        text: this.state.newText,
-        id: localStorage.getItem('sessionID')
-      })
+      headers: {},
+      body: data,
     })
       .then(response => {
         if(response.ok) {
           response.json().then(data => {
             if(data && data.status === 'success') {
               // import was started on server
-              this.context.handleNotificationChange(true, 'Der Text wurde zur Datenbank hinzugefügt.', 'import', 'success');
-              this.props.requestNewAuthors();
-              this.props.requestNewLog();
+              this.context.handleNotificationChange(true, 'Der/die Text/e wurde/n zur Datenbank hinzugefügt.', 'import', 'success');
+              this.updateLogs();
 
             } else {
-              this.context.handleNotificationChange(true, 'Der Text konnte nicht hinzugefügt werden.', 'import', 'error');
+              this.context.handleNotificationChange(true, 'Der Upload konnte nicht hinzugefügt werden.', 'import', 'error');
             }
           })
         } else {
-          this.context.handleNotificationChange(true, 'Der Text konnte nicht hinzugefügt werden.', 'import', 'error');
+          this.context.handleNotificationChange(true, 'Der Upload nicht hinzugefügt werden.', 'import', 'error');
         }
         this.handleChange('loading', false);
       })
       .catch(error => {
-        this.context.handleNotificationChange(true, 'Der Import konnte auf dem Server nicht gestartet werden.', 'import', 'error');
+        this.context.handleNotificationChange(true, 'Der Upload konnte nicht hinzugefügt werden.', 'import', 'error');
         this.handleChange('loading', false);
       });
   };
 
   render() {
     const {classes} = this.props;
-    const {newText, loading, lastImportTime, importStatus} = this.state;
-
-    // show example for how entered text should be formatted
-    const FormatExample = () => (
-      <div>
-        <Typography>
-          Bitte geben Sie Texte im folgenden Format ein (\n gibt einen Zeilenumbruch an):
-        </Typography><br/>
-        <Typography className={classes.newLine} color={'primary'}>
-          {`{"metadata": {
-            "type": "poem",
-            "author": "Guido Zernatto",
-            "title": "18 Gedichte",
-            "booktitle": "... kündet laut die Zeit",
-            "publisher": "Stiasny Verlag",
-            "editor": "Hans Brunmayr",
-            "year": "1961"
-          },
-            "content": {
-            "Sommerabend": "Gegen neun verdreht der Wind
-            \\n Über uns den Zug der Wolkenfläume
-            \\n Und fällt tiefer unten in die Bäume,
-            \\n Die schon schattenlos im Dämmer sind.
-            \\n In den Gräsern ist noch Ruh
-            \\n Und die abendliche Feuchte zeigt sich
-            \\n Kühl. Aus einem Fenster neigt sich
-            \\n Mein Gesicht der Erde zu."
-          }
-          }`}
-        </Typography>
-      </div>
-    );
+    const {loading, lastImportTime, importStatus} = this.state;
 
     return (
       <div>
@@ -218,41 +188,30 @@ class TextManagement extends Component {
         </div>
         <Divider variant='middle' className={classes.divider}/><br/>
         <div className={classes.addTextContainer}>
-          <Typography variant={'h6'} color={'primary'}>Text hinzufügen</Typography><br/>
-
-          <div className={classes.flexContainer}>
-            <TextField
-              variant='outlined'
-              multiline={true}
-              className={classes.textInput}
-              value={newText}
-              onChange={this.handleTextChange}
-              disabled={loading}
-              placeholder={'Geben Sie hier einen Text ein, der dem Beispiel-Format entspricht.'}
-            />
-          </div>
+          <Typography variant={'h6'} color={'primary'}>Text(e) hinzufügen</Typography>
           <div className={classes.buttonsContainer}>
-            <Button
-              className={classes.importButton}
-              color={'primary'}
-              variant={'contained'}
-              disabled={loading}
-              onClick={this.requestAddText}
-            >
-              Text abschicken
-            </Button>
-            <Button
-              className={classes.secondaryButton}
-              color={'primary'}
-              variant={'text'}
-              disabled={loading}
-              onClick={this.emptyTextField}
-            >
-              Eingabe löschen
-            </Button>
+            <TextField
+              accept='.txt, .json'
+              InputProps={{root: classes.textInput}}
+              id="fileUpload"
+              multiple
+              type={'file'}
+              onChange={this.handleFileChange}
+              variant={'outlined'}
+            />
+            <div className={classes.flexContainer}>
+              <Button
+                className={classes.importButton}
+                variant={'contained'}
+                component={'span'}
+                color={'primary'}
+                disabled={loading}
+                onClick={this.requestAddText}
+              >
+                Dateien hochladen
+              </Button>
+            </div>
           </div>
-          <br/>
-          {FormatExample()}
           <br/>
         </div>
       </div>
@@ -282,14 +241,14 @@ const styles = theme => ({
     marginLeft: theme.spacing.unit,
   },
   textInput:{
-    width: 800 + theme.spacing.unit*12,
+    width: 380,
+    padding: theme.spacing.unit,
   },
   divider:{
     marginTop: theme.spacing.unit * 5,
     width: 800 + theme.spacing.unit*12,
   },
   buttonsContainer:{
-    display: 'flex',
     marginTop: theme.spacing.unit,
     marginBottom: theme.spacing.unit * 2,
   },
