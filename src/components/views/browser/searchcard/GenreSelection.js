e38c17adcb682bd1a37c1b77677dc5f011453a7a
Beriwan Tepe <tepe.1@win.tu-berlin.de>
2019-03-05 17:09:01 +0100

    fixed genre selection

--- src/components/views/browser/searchcard/GenreSelection.js
+++ src/components/views/browser/searchcard/GenreSelection.js
@@ -54,7 +54,7 @@ class GenreSelection extends React.PureComponent {
             renderValue={selected => selected.join(', ')}
             MenuProps={MenuProps}
           >
-            {genres && genres.map(name => (
+            {genres && genres.isArray && genres.map(name => (
               <MenuItem key={name} value={name}>
                 <Checkbox checked={genre.indexOf(name) > -1} disableRipple={true} color={"primary"}/>
                 <ListItemText primary={name} />
