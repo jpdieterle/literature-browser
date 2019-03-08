64769478eee74874d2a1c2845cb7cdefe6ac8ba4
Beriwan Tepe <tepe.1@win.tu-berlin.de>
2019-03-08 13:57:54 +0100

    fixed fetch

--- src/App.js
+++ src/App.js
@@ -245,7 +245,7 @@ class App extends React.Component {
                     )}/>
                     <Route path='/about' component={About}/>
                     <Route path='/scripts' render={() => (
-                      loggedIn? (<ScriptDownload requestStatus={this.requestUserStatus.bind(this)}/>) : (<Redirect to='/login'/>)
+                      loggedIn? (<ScriptDownload requestStatus={this.requestUserStatus}/>) : (<Redirect to='/login'/>)
                     )}/>
                     <Route path='/admin' render={() => (
                       (loggedIn && isAdmin)? (<Admin requestNewAuthors={this.requestAuthors} requestNewLog={this.requestLog} requestStatus={this.requestUserStatus}/>) : (<Redirect to='/' />)
