e835eb82d7577c0c6a0c6791acfd08b42a7def2c
Anna-Liza Beriwan Tepe <tepe.1@campus.tu-berlin.de>
2019-03-10 13:22:52 +0100

    fixed authors input

--- src/components/views/browser/searchcard/AuthorInput.js
+++ src/components/views/browser/searchcard/AuthorInput.js
@@ -85,7 +85,8 @@ class AuthorInput extends React.PureComponent {
     const inputLength = inputValue.length;
     let count = 0;
     return inputLength === 0 ? [] : this.props.authorsList.filter(author => {
-      const keep = (count < 15 && author.includes(inputValue)
+    if(author.toLowerCase().includes('goethe')) console.log('goethe: ', author.toLowerCase().includes(inputValue), this.state.selectedItems.indexOf(author) === -1);
+      const keep = (count < 15 && author.toLowerCase().includes(inputValue)
         && this.state.selectedItems.indexOf(author) === -1);
       if (keep) {
         count += 1;
