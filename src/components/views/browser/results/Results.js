9a88985d247e1be2f5f97946d55483266d743e90
Anna-Liza Beriwan Tepe <tepe.1@campus.tu-berlin.de>
2019-03-10 12:54:32 +0100

    Update Results.js

--- src/components/views/browser/results/Results.js
+++ src/components/views/browser/results/Results.js
@@ -57,6 +57,7 @@ class Results extends React.Component {
     if(getAll) {
       let a = document.createElement('a');
       a.href = '/backend/archive/' + format==='json'? 'corpusJson.zip' : 'corpusTxt.zip';
+      console.log('href for getAll: ', a.href);
       a.download = format==='json'? 'corpusJson' : 'corpusTxt';
       a.click();
     } else {
